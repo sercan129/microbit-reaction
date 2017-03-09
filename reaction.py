4f374e5606de0736320c90b2a664e13c41b4034a
Joshua Garnham <joshua.garnham@yahoo.co.uk>
2017-03-09 13:47:27 +0000

    Initial (complete) commit

---
+++ reaction.py
@@ -0,0 +1,35 @@
+# Add your Python code here. E.g.
+from microbit import *
+from random import randrange
+
+delay=1500 #ms
+factor=.85
+
+def choose_player():
+    east_player = randrange(0,2)
+    if east_player:
+        display.show(Image.ARROW_E)
+    else:
+        display.show(Image.ARROW_W)
+    return east_player
+
+def correct_button_pressed(echosen):
+    if echosen and button_b.was_pressed():
+        return True
+    elif echosen == 0 and button_a.was_pressed():
+        return True
+    else:
+        return False
+
+while delay > 1:
+    echosen = choose_player()
+    sleep(delay)
+    if not correct_button_pressed(echosen):
+        display.scroll("GAME OVER")
+        break
+    delay = delay * factor
+    for i in range(3):
+        display.show(Image.SQUARE)
+        sleep(100)
+        display.clear()
+        sleep(100)
